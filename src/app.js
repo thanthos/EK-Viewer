abf73fc781f49199887c875b3633e2f6b47fed32
James Tang <thanthos@yahoo.com>
2016-01-11 10:01:24 +0800

    Checkpoint Save
    Established connection with ES.

--- src/app.js
+++ src/app.js
@@ -3,15 +3,15 @@
  *
  * This is where you write your app.
  */
-'use strict';
 
 var UI = require('ui');
-var Vector2 = require('vector2');
+//var Vector2 = require('vector2');
 var base64 = require('base64');
 var Settings = require('settings');
 var ajax = require('ajax');
-var Clock = require('clock');
+//var Clock = require('clock');
 var queryString = 'q=_type:visualization&_source=false&fields=visState';
+var searchIndex = '.kibana/_search?';
 
 Settings.config({
   url:"http://pebble-config.herokuapp.com/config?title=ElasticSearch%20Pebble&fields=text_Username,password_Password,text_URL,text_Search" 
@@ -37,36 +37,46 @@ Settings.config({
   });
 
 var main = new UI.Card({title:'Pebble ElasticSearch'});
-
 var user= Settings.option("Username");
 var password= Settings.option("Password");
 var esURL = Settings.option("URL");
-var defaultSearch = Settings.option("defaultVisualization");
+//var defaultSearch = Settings.option("defaultVisualization");
+
+
+
+function elasticSearch(query, failure, success){
+  var hash = base64.encode(Settings.option("Username")+":"+Settings.option("Password"));
+  console.log("Ajax Authorization Basic "+hash);
+  console.log("Ajax url: "+esURL+"/"+searchIndex+queryString);
+
+  ajax( {
+    url: esURL+"/"+searchIndex+queryString ,
+    headers: {Authorization:"Basic "+hash} },
+    success,
+    failure
+  );
+}
 
 console.log(JSON.stringify(Settings.option()));
 
 if ( !user || !esURL ){
   main.body='Not Configured';
   main.show();
-}else{
-  
-  var hash = base64.encode(user+":"+password);
-  console.log("Attempting to Connect to Elastic Search ");
-  main.body="Displaying Result ";
-  ajax( {
-    url: esURL+"/"+queryString,
-    type: 'application/json; charset=UTF-8',
-    headers: "Basic "+hash
-  },
-  function(data, status, request) {
-    console.log('Success  ' + data.contents.quote);
-    
-  },
-  function(error, status, request) {
-    console.log('The ajax request failed: ' + error);
-  }
-);
-  
+}else{  
+  console.log("Attempting to Connect to Elastic Search Onload");
+  elasticSearch(queryString,
+    function(error, status, request) {
+      console.log('The ajax request failed: ' + JSON.stringify(error));
+      console.log('Request : ' + JSON.stringify(request));
+    },
+    function(data, status, request) {
+      console.log('Status : ' + JSON.stringify(status));
+      console.log('Payload : ' + JSON.stringify(data));}
+  );
+  main.body="Displaying Result";
+  main.show(); 
+  console.log("End");
+
 }
 
 main.on('click', 'up', function(e) {
@@ -91,38 +101,14 @@ main.on('click', 'up', function(e) {
 
 main.on('click', 'select', function(e) {
   var action = new UI.Card({title:'Pebble ElasticSearch'});
-  var hash = base64.encode(Settings.option("Username")+":"+Settings.option("Password"));
-  console.log("Attempting to Connect to Elastic Search ");
+  console.log("Attempting to Connect to Elastic Search On Demand");
   action.body="Displaying Result ";
-  ajax( {
-    url: esURL+"/"+queryString,
-    type: 'application/json; charset=UTF-8',
-    headers: "Basic "+hash
-  },
-  function(data, status, request) {
-    console.log('Success  ' + data.contents.quote);
-    
-  },
-  function(error, status, request) {
-    console.log('The ajax request failed: ' + error);
-    action.body='The ajax request failed: ' + error;
-    action.show();
-  }
-);
-  
-  
-                           
-  action.show();
-//   var wind = new UI.Window();
-//   var textfield = new UI.Text({
-//     position: new Vector2(0, 50),
-//     size: new Vector2(144, 30),
-//     font: 'gothic-24-bold',
-//     text: 'Text Anywhere!',
-//     textAlign: 'center'
-//   });
-//   wind.add(textfield);
-//   wind.show();
+  elasticSearch(queryString, 
+    function(error, status, request) {
+      console.log('The ajax request failed: ' + JSON.stringify(error)); },
+    function(data, status, request) {
+      console.log('Data : ' + JSON.stringify(data));});
+ action.show();
 });
 
 main.on('click', 'down', function(e) {
